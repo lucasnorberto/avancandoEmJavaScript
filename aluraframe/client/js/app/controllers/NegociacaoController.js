263533225a52c33db434e783ff2a791484c4470e
Lucas Norberto <lucasnorberto@gmail.com>
2022-08-05 01:12:59 -0300

    Update NegociacaoController.js

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,16 +1,32 @@
 class NegociacaoController {
   constructor() {
     let $ = document.querySelector.bind(document);
-    this.inputData = $("#data");
-    this.inputQuantidade = $("#quantidade");
-    this.inputValor = $("#valor");
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
   }
 
   adiciona(event) {
+
     event.preventDefault();
 
-    console.log(this.inputData.value);
-    console.log(this.inputQuantidade.value);
-    console.log(this.inputValor.value);
+    let data = new Date(
+        ...this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+    );
+
+    let negociacao = new Negociacao(
+        data,
+        this._inputQuantidade.value,
+        this._inputValor.value
+    );
+
+    let diaMesAno = negociacao.data.getDate()
+            + '/' + (negociacao.data.getMonth()+1)
+            + '/' + negociacao.data.getFullYear();
+
+    console.log(diaMesAno);
+
   }
 }
